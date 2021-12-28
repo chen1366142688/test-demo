ffdf48ca7956e5a79f9b5a2d5f71e5d1c8e71edc
chen1366142688 <1366142688@qq.com>
2021-12-28 17:36:30 +0800

    第二次提交

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 
 window.onload = function () { 
-  let box = document.getElementsByClassName('box')[0]
-  box.addEventListener('click', () => { 
-    box.innerHTML = '这真的是一个测试示例'
-  })
+let i = 0,
+timer = 0,
+  str = '这真的是一个测试示例,绝对是比珍珠还真的哦';
+let box = document.getElementsByClassName('box')[0]
+box.addEventListener('click', () => { 
+  typing()  
+})
+function typing () {
+  if (i <= str.length) {
+    box.innerHTML = str.slice(0, i++) + '_'
+    timer = setTimeout(typing, 200)
+  }
+  else {
+    box.innerHTML = str//结束打字,移除 _ 光标
+    clearTimeout(timer)
+  }
+}
+
 }
